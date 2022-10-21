bddc7a28d2e0656988c240651c81e24516d156eb
GarridoMatias <mgarrido@alumnos.exa.unicen.edu.ar>
2022-10-20 21:23:04 -0300

    create component game-list incluido en app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GameListComponent } from './game-list/game-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameListComponent
   ],
   imports: [
     BrowserModule
