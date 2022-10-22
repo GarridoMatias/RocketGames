74a49210b32cfafd47df1d998aecf54c2c509905
GarridoMatias <mgarrido@alumnos.exa.unicen.edu.ar>
2022-10-21 22:52:42 -0300

    se cargaron los juegos, primer uso de directivas

---
+++ src/app/game-list/Game.ts
@@ -0,0 +1,10 @@
+export interface Game{
+    nombre : string;
+    imagen : string;
+    descripcion: string;
+    tipo : string;
+    precio : number;
+    stock: number;
+    destacado : boolean;
+    agregado : boolean;
+}
